5d913c48aceba2f7b6ea7cfc20d60264d9ff8823
FelipeCarvajalRodriguez <50501964+FelipeCarvajalRodriguez@users.noreply.github.com>
2019-05-10 17:56:49 -0300

    Add files via upload

---
+++ RX.ino
@@ -0,0 +1,148 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <XBee.h>
+#include <SoftwareSerial.h>
+XBee xbee = XBee();
+//////////////////////////
+
+byte ledPin =  5; // Pino do LED
+boolean ledState = false; // Estado do LED: False = desligado ou true = ligado
+long prevMillis = 0; // Última mudança de estado
+long interval = 2000; // intervalo desejado para o blink
+
+//////////////////////////////////////////////////////
+
+//SoftwareSerial XBee(3,2);
+ZBRxResponse zbRx = ZBRxResponse();
+
+union u_tag {
+    uint8_t b[4];
+    float fval;
+} u;
+
+void setup () {
+  
+  Serial.begin(9600);
+  xbee.begin(Serial);
+  pinMode(ledPin, OUTPUT);
+  setTime(4,1,00,12,9,2018);
+
+  }
+
+void loop () {
+///////////////////////////////////////////
+  unsigned long curMillis = millis(); //millis atual
+  if(curMillis - prevMillis < interval) {
+    prevMillis = curMillis;  
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);}
+
+/////////////////////////////////
+
+time_t t = now();//Declaramos la variable time_t t
+String sample;
+xbee.readPacket();
+  if (xbee.getResponse().isAvailable()) {
+    //Serial.println("Packet available"); 
+    } 
+if (xbee.getResponse().isAvailable()) {
+      //Serial.println(xbee.getResponse().getApiId());
+      //Serial.println();
+      //Serial.print("Frame Type is ");
+      //Serial.println(xbee.getResponse().getApiId(), HEX);
+      }
+
+      if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+        xbee.getResponse().getZBRxResponse(zbRx);
+        /////////////////////////////////////////////////
+         // now that you know it's a receive packet
+        // fill in the values
+     
+      
+        // this is how you get the 64 bit address out of
+        // the incoming packet so you know which device
+        // it came from
+        //Serial.print("Got an rx packet from: ");
+        XBeeAddress64 senderLongAddress = zbRx.getRemoteAddress64();
+        //Serial.print(senderLongAddress.getMsb());
+        //Serial.print(" ");
+        //Serial.print(senderLongAddress.getLsb());
+      
+        // this is how to get the sender's
+        // 16 bit address and show it
+        uint16_t senderShortAddress = zbRx.getRemoteAddress16();
+        //Serial.print(" (");
+        //Serial.print(senderShortAddress);
+        //Serial.print(")");
+        //Serial.print(" ");
+      
+        // The option byte is a bit field
+        if (zbRx.getOption() & ZB_PACKET_ACKNOWLEDGED)
+            // the sender got an ACK
+          //Serial.println("packet acknowledged");
+        if (zbRx.getOption() & ZB_BROADCAST_PACKET)
+          // This was a broadcast packet
+          //Serial.println("broadcast Packet");
+        
+        //Serial.print("checksum is ");
+        //Serial.println(zbRx.getChecksum(), HEX);
+      
+        // this is the packet length
+        //Serial.print("packet length is ");
+        //Serial.print(zbRx.getPacketLength(), DEC);
+      
+        // this is the payload length, probably
+        // what you actually want to use
+        //Serial.print(", data payload length is ");
+        //Serial.println(zbRx.getDataLength(),DEC);
+      
+        // this is the actual data you sent
+        //Serial.println("Received Data: ");
+        //for (int i = 0; i < zbRx.getDataLength(); i++) {
+          //Serial.print(zbRx.getData()[i]);
+          //Serial.print(' ');
+
+       
+        ///////////////////////////////////////////////
+       // for (int i = 0; i < zbRx.getDataLength(); i++) {
+        //sample += (char)zbRx.getData(i);
+       
+          u.b[0]= zbRx.getData(0);
+          u.b[1]= zbRx.getData(1);
+          u.b[2]= zbRx.getData(2);
+          u.b[3]= zbRx.getData(3);
+       //}
+
+
+        
+//////////////////////////////////////////////////////////////////
+        //Serial.println(sample);
+
+        //Serial.print("Temperatura: ");
+        //Serial.print("Temperatura: ");
+        Serial.print(senderLongAddress.getLsb());
+        Serial.print(",");
+        Serial.print(u.fval);
+        Serial.print(",");
+        Serial.print(day(t));
+    Serial.print(+ "/") ;
+    Serial.print(month(t));
+    Serial.print(+ "/") ;
+    Serial.print(year(t)); 
+    Serial.print( " ") ;
+    Serial.print(hour(t));  
+    Serial.print(+ ":") ;
+    Serial.print(minute(t));
+    Serial.print(":") ;
+    Serial.println(second(t));
+        
+        //Serial.println(xbee.getResponse().getApiId());
+         /////////////////////////
+      
+
+         ////////////////////////
+      }
+
+      delay(10000);
+}
+
---
+++ TX.ino
@@ -0,0 +1,98 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <TMP36.h>
+#include <XBee.h>
+//TMP36 tmp36(A0,5);
+XBee xbee = XBee();
+
+//////////////////////////
+
+byte ledPin =  5; // Pino do LED
+boolean ledState = false; // Estado do LED: False = desligado ou true = ligado
+long prevMillis = 0; // Última mudança de estado
+long interval = 2000; // intervalo desejado para o blink
+
+//////////////////////////////////////////////////////
+const int pinDatosDQ = 2;
+
+// Instancia a las clases OneWire y DallasTemperature
+OneWire oneWireObjeto(pinDatosDQ);
+DallasTemperature sensorDS18B20(&oneWireObjeto);
+uint8_t payload[4] ={0,0,0,0};
+
+union u_tag {
+    uint8_t b[4];
+    float fval;
+} u;
+ 
+//XBeeAddress64 addr64 = XBeeAddress64(0x0013a200,0x041085ad3);
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200,0x041085f96);
+ZBTxRequest tx = ZBTxRequest(addr64, payload, sizeof(payload));
+
+int statusLed = 13;
+int errorLed = 13;
+ 
+void flashLed(int pin, int times, int wait) {
+ 
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+ 
+    if (i + 1 < times) {
+      delay(wait);
+    }
+  }
+}
+
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+// Iniciamos el bus 1-Wire
+    sensorDS18B20.begin(); 
+    pinMode(ledPin, OUTPUT);
+
+  
+}
+
+void loop() {
+
+
+
+  ///////////////////////////////////////////
+  unsigned long curMillis = millis(); //millis atual
+  if(curMillis - prevMillis < interval) {
+    prevMillis = curMillis;  
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);}
+
+/////////////////////////////////
+  //float tmp = tmp36.getTempC();
+sensorDS18B20.requestTemperatures();
+
+float tmp = sensorDS18B20.getTempCByIndex(0);
+    
+  if (!isnan(tmp)) {
+    // same for the temperature
+    u.fval = tmp;
+    for (int i=0;i<4;i++){
+      payload[i]=u.b[i];
+    }
+  
+  }
+  //payload[0]='T';
+  //payload[1]='e';
+  //payload[2]='m';
+  //payload[3]='p';
+    
+  xbee.send(tx);
+  flashLed(statusLed, 1, 100);
+  //Serial.println(tmp);
+  delay (60000);
+
+}
+
+
+
